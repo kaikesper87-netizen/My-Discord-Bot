4781b3f27498d1f5d6ba695432c340c9c7fa8503
kaikesper87-netizen <kaikesper87@gmail.com>
2025-10-29 22:44:57 +0800

    Create start.js

---
+++ src/commands/start.js
@@ -0,0 +1,8 @@
+// src/commands/start.js
+export async function execute(interaction, client) {
+    await interaction.reply("⚠️ /start command not implemented yet!");
+}
+
+export async function handleComponent(interaction, client) {
+    await interaction.reply("⚠️ Component not implemented yet!");
+}
