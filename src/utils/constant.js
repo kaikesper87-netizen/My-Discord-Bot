ead1eda59485d1c8eb224f1097984d6b19a878cd
kaikesper87-netizen <kaikesper87@gmail.com>
2025-10-29 22:59:26 +0800

    Create constant.js

---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+// src/utils/constants.js
+
+export const ELEMENTS = ["Fire", "Water", "Wind", "Lightning", "Earth", "Light", "Dark", "Ice", "Poison", "Arcane", "Nature", "Metal", "Divine"];
+
+// You can move other constants here later, like itemDatabase, passives, etc.
