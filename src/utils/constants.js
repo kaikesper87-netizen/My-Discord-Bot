61246ba81892791b703af50e7740f594eeec37bd
kaikesper87-netizen <kaikesper87@gmail.com>
2025-10-30 09:09:06 +0800

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,19 @@
 // src/utils/constants.js
-
 export const ELEMENTS = ["Fire", "Water", "Wind", "Lightning", "Earth", "Light", "Dark", "Ice", "Poison", "Arcane", "Nature", "Metal", "Divine"];
 
-// You can move other constants here later, like itemDatabase, passives, etc.
+// Add this:
+export const ELEMENT_PASSIVES = {
+    Fire: 'Burn chance +10%',
+    Water: 'Heal +5%',
+    Wind: 'Evasion +5%',
+    Lightning: 'Critical +5%',
+    Earth: 'Defense +5%',
+    Light: 'Holy damage +5%',
+    Dark: 'Lifesteal +5%',
+    Ice: 'Freeze chance +5%',
+    Poison: 'Poison damage +5%',
+    Arcane: 'Mana regen +5%',
+    Nature: 'HP regen +5%',
+    Metal: 'Attack +5%',
+    Divine: 'All stats +2%'
+};
