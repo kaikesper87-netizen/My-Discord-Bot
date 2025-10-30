0c8152af949c32e4da9e0bd1c8b294a61da6b035
kaikesper87-netizen <kaikesper87@gmail.com>
2025-10-30 09:45:55 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,1 +1,17 @@
+// In-memory database (JSON-based can be added later)
+export const players = {};
+export const guilds = {};
+export const battles = {};
 
+// Player helper functions
+export function getPlayer(id) {
+    return players[id] || null;
+}
+
+export function setPlayer(id, data) {
+    players[id] = data;
+}
+
+export function getAllPlayers() {
+    return players;
+}
